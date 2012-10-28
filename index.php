55956d35d3481246774c1fc96706b1f19bd56706
Jørgen Rieck <jurgemaister@gmail.com>
2012-10-28 13:19:18 -0600

    Added web files

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>Ballmer Peak Oslo</title>
+		<link rel="icon" type="image/png" href="favicon.png">
+		<style type="text/css">
+			@font-face {
+				font-family: Techniqe;
+				src: url('techniqu.ttf');
+			}
+
+			body {
+				background: #139CCE;
+				color: #FFF;
+			}
+
+			h1, h2 {
+				font-family: Techniqe;
+			}
+
+			h1 {
+				font-size: 40px;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Ballmer Peak Oslo</h1>
+	</body>
+</html>
