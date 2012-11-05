7ca6457772b56d6a86b2759418c24e181ce3ed0b
Jørgen Rieck <jurgemaister@gmail.com>
2012-11-05 15:00:04 +0100

    Added Piwik tracking code

--- index.php
+++ index.php
@@ -75,6 +75,19 @@ function get_signup_count() {
 		<?else: ?>
 		<p>Vi er desverre fulltegnet for denne gang, men følg med! Det kommer alltid en neste :)</p>
 		<?endif;?>
+		
+		<!-- Piwik --> 
+		<script type="text/javascript">
+		var pkBaseURL = (("https:" == document.location.protocol) ? "https://stats.rixim.no/" : "http://stats.rixim.no/");
+		document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+		</script><script type="text/javascript">
+		try {
+		var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 2);
+		piwikTracker.trackPageView();
+		piwikTracker.enableLinkTracking();
+		} catch( err ) {}
+		</script><noscript><p><img src="http://stats.rixim.no/piwik.php?idsite=2" style="border:0" alt="" /></p></noscript>
+		<!-- End Piwik Tracking Code -->
 	</body>
 </html>
 
